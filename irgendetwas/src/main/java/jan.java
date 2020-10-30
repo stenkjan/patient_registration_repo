fe7797fc763bd7d9571c966c5c17cf9fda6ce2f0
jst01 <jst01@DESKTOP-ARSUN0F.fritz.box>
2020-10-30 11:57:50 +0100

    main.class

---
+++ irgendetwas/src/main/java/jan.java
@@ -0,0 +1,15 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author jst01
+ */
+public class jan {
+    public static void main(String[] args){
+        System.out.println("Ausgabe aus der main()-Methode");
+    }
+}
