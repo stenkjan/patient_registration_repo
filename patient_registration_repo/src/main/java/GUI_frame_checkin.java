475a2726426a65c16b86d4131ed3959bda7ece55
maxgr <max.gritsch@outlook.com>
2020-11-04 10:52:14 +0100

    Gui Frame Runnable mit Override

--- patient_registration_repo/src/main/java/GUI_frame_checkin.java
+++ patient_registration_repo/src/main/java/GUI_frame_checkin.java
@@ -127,6 +127,7 @@ public class GUI_frame_checkin extends javax.swing.JFrame {
 
         /* Create and display the form */
         java.awt.EventQueue.invokeLater(new Runnable() {
+            @Override
             public void run() {
                 new GUI_frame_checkin().setVisible(true);
             }
